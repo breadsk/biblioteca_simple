ad804d521b61eeb5840a6f5844b46bfe86dc279e
Nicolas C <nicolas.programador@gmail.com>
2025-04-17 09:35:41 -0400

    First commit

---
+++ src/main/java/com/example/bioblite_simple/BiobliteSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bioblite_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiobliteSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiobliteSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bioblite_simple/BiobliteSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bioblite_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiobliteSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
